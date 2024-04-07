0c6c65ecc1ec29cd50e7bec6ae791ce7ec02f62a
Sarthak Bansal <sarthakbansal087@gmail.com>
2024-04-07 15:31:39 +0530

    starting the project

---
+++ frontend/scholarlystrength/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>HELLO</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
